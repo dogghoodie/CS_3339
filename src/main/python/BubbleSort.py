afce59684f908014441e6228ef3d80957ebdc81d
dogghoodie <dogghoodie@gmail.com>
2024-03-20 18:09:05 -0500

    python cpu times

--- src/main/python/BubbleSort.py
+++ src/main/python/BubbleSort.py
@@ -1,3 +1,5 @@
+import time
+
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
@@ -6,7 +8,17 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 arr = [10, 7, 9, 8, 5, 6, 4, 2, 1, 3]
+
+cpu_start_time = time.process_time()
+
 bubble_sort(arr)
 
+cpu_end_time = time.process_time()
+
+cpu_elapsed_time = (cpu_end_time - cpu_start_time)
+
 print("Sorted array is:", arr)
+print(f"\nStart Time : {cpu_start_time:.5f}")
+print(f"End Time : {cpu_end_time:.5f}")
+print(f"Elapsed Time : {cpu_elapsed_time:.5f}")
 
