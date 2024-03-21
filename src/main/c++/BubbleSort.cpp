c009c409d607d57260f667276391bc4e8a7b8814
dogghoodie <dogghoodie@gmail.com>
2024-03-21 03:36:12 -0500

    output cleanup

--- src/main/c++/BubbleSort.cpp
+++ src/main/c++/BubbleSort.cpp
@@ -32,8 +32,8 @@ int main() {
 
     cout << "Sorted array: [";
     for (int i = 0; i < n; i++){
-		if (i < n-1) {
-		cout << arr[i] << " ";
+		if (i < n - 1) {
+		cout << arr[i] << ", ";
 		} else {
 			cout << arr[i] << "]" << endl;
 		}
