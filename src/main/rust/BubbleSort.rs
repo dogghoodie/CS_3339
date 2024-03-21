e6988fee2422854df53ca140016d86ab00dac8f3
dogghoodie <dogghoodie@gmail.com>
2024-03-21 03:44:37 -0500

    rust cleanup

--- src/main/rust/BubbleSort.rs
+++ src/main/rust/BubbleSort.rs
@@ -18,5 +18,8 @@ fn main() {
     let mut arr = [10, 7, 9, 8, 5, 6, 4, 2, 1, 3];
     bubble_sort(&mut arr);
     println!("Sorted array: {:?}", arr);
+    println!("CPU Start Time : ");
+    println!("CPU End Time : ");
+    println!("CPU Elapsed Time : ");
 }
 
